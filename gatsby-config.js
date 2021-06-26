1208175dab2ef6cdd3c3f187468cab26ad7b0818
Alex <dev@salex.pro>
2021-06-26 23:38:29 +0300

    move percy to gh actions

--- gatsby-config.js
+++ gatsby-config.js
@@ -59,7 +59,6 @@ module.exports = {
       },
     },
     `gatsby-plugin-gatsby-cloud`,
-    `gatsby-plugin-percy`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
