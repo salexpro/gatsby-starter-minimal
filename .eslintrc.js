805023189ee48caedcb51384da921e29d0a4f4ff
Savelii Lukash <SaveliiLukash@gmail.com>
2021-11-02 17:18:29 +0200

    feat: add eslint-plugin-json

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,12 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
+  extends: [
+    'plugin:react/recommended',
+    'plugin:json/recommended',
+    'airbnb',
+    'prettier',
+  ],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
