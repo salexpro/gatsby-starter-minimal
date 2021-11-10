80f5baff78be5e382c6f1a9a34c26f19c46a7eca
Alex <dev@salex.pro>
2021-11-10 15:45:21 +0200

    feat: remove trailing slashes by default

--- gatsby-config.js
+++ gatsby-config.js
@@ -60,6 +60,7 @@ module.exports = {
         ],
       },
     },
+    `gatsby-plugin-remove-trailing-slashes`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
