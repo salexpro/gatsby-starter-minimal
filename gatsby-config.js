bf1cfa0b2aacac47c03d04819ac4092e50df2d90
Alex <dev@salex.pro>
2022-03-08 00:00:28 +0000

    feat: add minify production css hack

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,5 @@
+const rnd = () => Math.random().toString(36).substring(2, 3)
+
 module.exports = {
   /*
   Experimental flags that increase DX and build times with different technics (may require to use `yarn clean` time to time)
@@ -13,35 +15,24 @@ module.exports = {
   },
   plugins: [
     // https://www.gatsbyjs.com/plugins/gatsby-plugin-webpack-bundle-analyser-v2/
-    `gatsby-plugin-webpack-bundle-analyser-v2`,
+    // `gatsby-plugin-webpack-bundle-analyser-v2`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`,
-    `gatsby-plugin-minify-classnames`,
-    // {
-    //   resolve: `gatsby-plugin-purgecss`,
-    //   options: {
-    //     printRejected: true, // Print removed selectors and processed file names
-    //     develop: true, // Enable while using `gatsby develop`
-    //     // tailwind: true, // Enable tailwindcss support
-    //     // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
-    //     // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
-    //     // purgeCSSPaths: ['./node_modules/react-bootstrap/**/*.js'],
-    //     purgeCSSOptions: {
-    //       // https://purgecss.com/configuration.html#options
-    //       safelist: {
-    //         standard: ['container', /dropdown$/, /nav$/, /btn$/],
-    //       }, // Don't remove this selector
-    //     },
-    //   },
-    // },
+    {
+      resolve: `gatsby-plugin-minify-classnames`,
+      options: {
+        enable: process.env.GATSBY_VERCEL_ENV === 'production',
+        prefix: rnd(),
+        suffix: rnd(),
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-image`,
     // {
     //   resolve: `gatsby-source-filesystem`,
     //   options: {
-    //     name: `images`,
-    //     path: `${__dirname}/data/images`,
+    //     path: `${__dirname}/data`,
     //   },
     // },
     {
