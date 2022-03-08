8c483f881132adefc35a96455a4931f3f0b08063
Alex <dev@salex.pro>
2022-03-08 00:03:15 +0000

    fix: replace trailing slash plugin to option

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@ module.exports = {
   // flags: {
   //   FAST_DEV: true,
   // },
+  trailingSlash: 'never',
   siteMetadata: {
     title: `Gatsby Starter`,
     description: `Description`,
@@ -48,7 +49,6 @@ module.exports = {
         ],
       },
     },
-    `gatsby-plugin-remove-trailing-slashes`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
