fde77854f2a1dd3172553d767d1159dbdd6b21d4
Alex <dev@salex.pro>
2023-04-07 18:03:34 +0100

    chore: test move to npm

--- gatsby-config.js
+++ gatsby-config.js
@@ -65,7 +65,6 @@ module.exports = {
         ],
       },
     },
-    `gatsby-plugin-webpack-bundle-analyser-v2`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
